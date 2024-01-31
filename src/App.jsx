14d0b0059fc5ff3e6816d770f5034e025cd69490
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-01-31 15:32:10 -0500

    implement basic routes, ContactPage.jsx, Homepages.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
-import React from "react";
 import "./App.css";
 // import CampsiteCard from "./features/campsites/CampsiteCard.js";
 import CampsitesDirectoryPage from "./pages/CampsitesDirectoryPage.jsx";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import ContactPage from "./pages/ContactPage.jsx";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
 	return (
 		<div className="App">
 			<Header />
-			<CampsitesDirectoryPage />
+			<Routes>
+				<Route path="/" element={<HomePage />} />
+				<Route path="contact" element={<ContactPage />} />
+				<Route path="directory" element={<CampsitesDirectoryPage />} />
+			</Routes>
 			<Footer />
 		</div>
 	);
