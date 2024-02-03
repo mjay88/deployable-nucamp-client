cbdff2784f7b4f773783994ed524fad65390cf66
Michael Jarrett <mwilliamkjarrett@gmail.com>
2024-02-03 17:19:05 -0600

    AboutPage.jsx, Partner.jsx, PartnerList.jsx, update routes in App.js

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import HomePage from "./pages/HomePage.jsx";
 import ContactPage from "./pages/ContactPage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
 import { Route, Routes } from "react-router-dom";
 import CampsiteDetailPage from "./pages/CampsiteDetailPage.jsx";
 
@@ -17,6 +18,7 @@ function App() {
 				<Route path="contact" element={<ContactPage />} />
 				<Route path="directory" element={<CampsitesDirectoryPage />} />
 				<Route path="directory/:campsiteId" element={<CampsiteDetailPage />} />
+				<Route path="about" element={<AboutPage />} />
 			</Routes>
 			<Footer />
 		</div>
---
+++ src/features/partners/PartnersList.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import Partner from "./Partner";
+import { Col } from "reactstrap";
+import { selectAllPartners } from "./partnersSlice";
+
+const PartnersList = () => {
+	const partners = selectAllPartners();
+	return (
+		<Col className="mt-4">
+			{partners.map((partner) => {
+				return (
+					<div className="d-flex mb-5" key={partner.id}>
+						<Partner partner={partner} />
+					</div>
+				);
+			})}
+		</Col>
+	);
+};
+
+export default PartnersList;
